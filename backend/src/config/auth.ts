b7874fb0e905cf79fb0218beb25fc8975d3b7813
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-12-31 14:35:28 -0300

    chore: usa variáveis de ambiente para JWT no auth.ts

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,7 @@
 export default {
   secretToken: process.env.JWT_SECRET || "default_secret",
-  secretRefreshToken: process.env.JWT_REFRESH_SECRET || "default_refresh_secret",
+  secretRefreshToken:
+    process.env.JWT_REFRESH_SECRET || "default_refresh_secret",
   expiresInToken: "15m",
   expiresInRefreshToken: "30d",
   expiresRefreshTokenDays: 30,
