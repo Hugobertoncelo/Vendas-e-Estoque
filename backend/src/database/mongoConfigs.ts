99e1c5627b0d4c0c0b09af077a2fbab8fcd1aa0d
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-12-31 15:32:46 -0300

    fix: adiciona bufferCommands: false na conexão mongoose para evitar buffering em serverless

--- backend/src/database/mongoConfigs.ts
+++ backend/src/database/mongoConfigs.ts
@@ -5,7 +5,7 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 const mongoURL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@stockcontrol.edrkre6.mongodb.net/stockcontrol?retryWrites=true&w=majority`;
 
-mongoose.connect(mongoURL);
+mongoose.connect(mongoURL, { bufferCommands: false });
 mongoose.connection
   .on(
     "error",
