51d7d5cfdbdbbc1fbf2ca39a5322639be9e9d0d2
Hugobertoncelo <hugobertoncelo@gmail.com>
2026-01-02 17:16:55 -0300

    chore: add try/catch to api.js for import error logging on Vercel

--- backend/dist/src/api.js
+++ backend/dist/src/api.js
@@ -1,2 +1,15 @@
-const handler = require('./index.js');
-module.exports = handler;
+try {
+  const handler = require('./index.js');
+  module.exports = handler;
+} catch (err) {
+  console.error('[FATAL][API.JS] Erro ao importar handler:', err);
+  module.exports = (req, res) => {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+      error: 'Erro fatal ao importar handler',
+      details: err.message,
+      stack: err.stack,
+    }));
+  };
+}
