1188ace15a8733578534cd9579270ce57d241a0d
Hugobertoncelo <hugobertoncelo@gmail.com>
2026-01-02 13:52:58 -0300

    fix: adiciona verificação obrigatória para MONGO_DATABASE em mongoConfigs.ts

--- backend/src/database/mongoConfigs.ts
+++ backend/src/database/mongoConfigs.ts
@@ -10,6 +10,11 @@ console.log(
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 const MONGO_DATABASE = process.env.MONGO_DATABASE;
+if (!MONGO_DATABASE) {
+  throw new Error(
+    "A variável de ambiente MONGO_DATABASE não está definida! (mongoConfigs.ts)"
+  );
+}
 const mongoURL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@stockcontrol.edrkre6.mongodb.net/${MONGO_DATABASE}?appName=stockcontrol`;
 
 console.log("[DEBUG] String de conexão MongoDB:", mongoURL);
